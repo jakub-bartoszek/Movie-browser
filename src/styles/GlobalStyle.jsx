230370e29dc068f800f22af54a923299f1e714bf
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-11-06 20:15:59 +0100

    initial template for project

---
+++ src/styles/GlobalStyle.jsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html{
+    box-sizing: border-box
+  }
+
+  *, ::after, ::before{
+    margin: 0;
+    box-sizing: inherit
+  }
+
+  body{
+    font-family: 'Inter', sans-serif
+  }
+`;
---
+++ src/styles/theme.jsx
@@ -0,0 +1,16 @@
+const colorNames = {
+	white: "#FFFFFF",
+	black: "#000000"
+};
+
+const common = {};
+
+export const themeLight = {
+	...common,
+	colors: {}
+};
+
+export const themeDark = {
+	...common,
+	colors: {}
+};

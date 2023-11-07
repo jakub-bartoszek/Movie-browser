4a203ef8abb429d70b774ccbb21c2fcc5933009a
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-11-07 21:05:07 +0100

    created basic theme

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -1,16 +1,31 @@
 const colorNames = {
-	white: "#FFFFFF",
-	black: "#000000"
+	White: "#FFFFFF",
+	Black: "#000000",
+	Woodsmoke: "#18181B",
+	Waterloo: "#7E839A"
 };
 
-const common = {};
-
-export const themeLight = {
-	...common,
-	colors: {}
+export const theme = {
+	breakpoints: {
+		s: "600px",
+		m: "900px",
+		l: "1200px",
+		xl: "1536px"
+	},
+	colors: {
+		site: {
+			background: colorNames.White,
+			text: colorNames.Black
+		},
+		navigation: {
+			background: colorNames.Woodsmoke,
+			text: colorNames.White
+		},
+		searchBar: {
+			background: colorNames.White,
+			text: colorNames.Black,
+			mutedText: colorNames.Waterloo
+		}
+	}
 };
 
-export const themeDark = {
-	...common,
-	colors: {}
-};

416fde591ef1aebe0c89d187b0d9ee31fa717d39
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2023-11-08 01:51:29 +0100

    make tile component, add to movie page

--- src/components/common/Tile/Tile.jsx
+++ src/components/common/Tile/Tile.jsx
@@ -1,5 +1,33 @@
+import {
+  Container,
+  Content,
+  Header,
+  Image,
+  Year,
+  Release,
+  ReleaseDate,
+  GreyRelease
+} from "./Styled"
+import poster from "./images/poster.png"
+
 export const Tile = () => {
   return (
-    <div>Tile</div>
-  )
-}
+    <>
+      <Container>
+        <Image src={poster} alt="" />
+        <Content>
+          <Header> Mulan</Header>
+          <Year> 2020 </Year>
+          <Release>
+            <GreyRelease>Production: </GreyRelease>
+            China, United States of America
+          </Release>
+          <ReleaseDate>
+            <GreyRelease>Release date:</GreyRelease>
+            24.10.2020
+          </ReleaseDate>
+        </Content>
+      </Container>
+    </>
+  );
+};

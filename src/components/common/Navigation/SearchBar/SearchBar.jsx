95d69c295bdc7853622c65c0ec01ad3eb1156dda
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-11-12 20:00:53 +0100

    controlled input in SearchBar

--- src/components/common/Navigation/SearchBar/SearchBar.jsx
+++ src/components/common/Navigation/SearchBar/SearchBar.jsx
@@ -1,10 +1,24 @@
 import { SearchInput, StyledSearchIcon, Wrapper } from "./styled";
+import { useDispatch, useSelector } from "react-redux";
+import {
+	selectSearchQuery,
+	setSearchQuery
+} from "../../../../utils/redux/moviesSlice";
 
 export const SearchBar = () => {
+	const dispatch = useDispatch();
+	const searchQuery = useSelector(selectSearchQuery);
+
 	return (
 		<Wrapper>
 			<StyledSearchIcon />
-			<SearchInput placeholder="Search for movies..." />
+			<SearchInput
+				placeholder="Search for movies..."
+				value={searchQuery}
+				onChange={(e) => {
+					dispatch(setSearchQuery(e.target.value));
+				}}
+			/>
 		</Wrapper>
 	);
 };

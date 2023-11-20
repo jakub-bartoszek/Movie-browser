931bb92cd69968cc1c91c78e8c0c0c66b83593be
Szymon Basiul <szymon.basiul@wp.pl>
2023-11-20 20:44:21 +0100

    Created simple no result view.

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -2,92 +2,90 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { SmallTile } from "../../components/common/Tile/SmallTile/SmallTile";
 import {
-	fetchPopularMovies,
-	fetchSearchResults,
-	selectMovies,
-	selectSearchQuery,
-	selectStatus
+  fetchPopularMovies,
+  fetchSearchResults,
+  selectMovies,
+  selectSearchQuery,
+  selectStatus,
 } from "../../utils/redux/moviesSlice";
 import { nanoid } from "nanoid";
 
 import {
-	Button,
-	Container,
-	Content,
-	Header,
-	Pagination,
-	StyledLeftIcon,
-	StyledRightIcon
+  Button,
+  Container,
+  Content,
+  Header,
+  Pagination,
+  StyledLeftIcon,
+  StyledRightIcon,
 } from "./styled";
+import { NoResult } from "../NoResult/NoResult";
 
 export default function Movies() {
-	const dispatch = useDispatch();
-	const movies = useSelector(selectMovies);
-	const searchQuery = useSelector(selectSearchQuery);
-	const status = useSelector(selectStatus);
+  const dispatch = useDispatch();
+  const movies = useSelector(selectMovies);
+  const searchQuery = useSelector(selectSearchQuery);
+  const status = useSelector(selectStatus);
 
-	useEffect(() => {
-		if (searchQuery) {
-			dispatch(fetchSearchResults(searchQuery));
-		} else {
-			dispatch(fetchPopularMovies());
-		}
-	}, [searchQuery, dispatch]);
+  useEffect(() => {
+    if (searchQuery) {
+      dispatch(fetchSearchResults(searchQuery));
+    } else {
+      dispatch(fetchPopularMovies());
+    }
+  }, [searchQuery, dispatch]);
 
-	return (
-		<Container>
-			{status !== "error" && (
-				<Header>
-					{!searchQuery
-						? `Popular movies`
-						: (searchQuery && movies.length) > 0
-						? `Search results for "${searchQuery}" (${movies.length})`
-						: `Sorry, there are no results for "${searchQuery}"`}
-				</Header>
-			)}
-			{
-				{
-					loading: <p>Loading...</p>, //Just to demo status pages
-					error: <p>Error!</p>,
-					success: (
-						<>
-							{movies.length > 0 ? (
-								<>
-									<Content>
-										{movies?.map((movie) => (
-											<SmallTile
-												key={nanoid()}
-												movie={movie}
-											/>
-										))}
-									</Content>
-									<Pagination>
-										<Button>
-											<StyledLeftIcon />
-											<StyledLeftIcon />
-										</Button>
-										<Button>
-											<StyledLeftIcon />
-										</Button>
-										<span>
-											Page <strong>1</strong> of <strong>500</strong>
-										</span>
-										<Button>
-											<StyledRightIcon />
-										</Button>
-										<Button>
-											<StyledRightIcon />
-											<StyledRightIcon />
-										</Button>
-									</Pagination>
-								</>
-							) : (
-								<div>No results</div> //Just to demo no results page
-							)}
-						</>
-					)
-				}[status]
-			}
-		</Container>
-	);
+  return (
+    <Container>
+      {status !== "error" && (
+        <Header>
+          {!searchQuery
+            ? `Popular movies`
+            : (searchQuery && movies.length) > 0
+            ? `Search results for "${searchQuery}" (${movies.length})`
+            : `Sorry, there are no results for "${searchQuery}"`}
+        </Header>
+      )}
+      {
+        {
+          loading: <p>Loading...</p>, //Just to demo status pages
+          error: <p>Error!</p>,
+          success: (
+            <>
+              {movies.length > 0 ? (
+                <>
+                  <Content>
+                    {movies?.map((movie) => (
+                      <SmallTile key={nanoid()} movie={movie} />
+                    ))}
+                  </Content>
+                  <Pagination>
+                    <Button>
+                      <StyledLeftIcon />
+                      <StyledLeftIcon />
+                    </Button>
+                    <Button>
+                      <StyledLeftIcon />
+                    </Button>
+                    <span>
+                      Page <strong>1</strong> of <strong>500</strong>
+                    </span>
+                    <Button>
+                      <StyledRightIcon />
+                    </Button>
+                    <Button>
+                      <StyledRightIcon />
+                      <StyledRightIcon />
+                    </Button>
+                  </Pagination>
+                </>
+              ) : (
+                <NoResult /> //Just to demo no results page
+              )}
+            </>
+          ),
+        }[status]
+      }
+    </Container>
+  );
 }
---
+++ src/pages/NoResult/styled.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import { ReactComponent as Paperplane } from "../../assets/icons/noresult/paperplane.svg";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  margin-top: 0;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Image = styled(Paperplane)`
+  width: 100%;
+  height: 100%;
+`;

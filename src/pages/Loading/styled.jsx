e186fa7bc12ca2f4bcf0f9805fc938eaa6021fe1
Szymon Basiul <szymon.basiul@wp.pl>
2023-11-21 21:28:20 +0100

    Animation added.

--- src/pages/Loading/styled.jsx
+++ src/pages/Loading/styled.jsx
@@ -1,8 +1,18 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { ReactComponent as Loader } from "../../assets/icons/loader/loader.svg";
 
 export const Image = styled(Loader)`
   align-self: center;
   width: 91px;
   height: 91px;
+  animation: spiner 0.7s linear infinite;
+
+  @keyframes spiner {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;

70a1878a349a63d5eb162a41b2fa0f74567b16a4
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-11-21 18:17:48 +0100

    Wrapped routes in scrollable container

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Movies from "./pages/Movies/Movies";
 import People from "./pages/People/People";
 import { Navigation } from "./components/common/Navigation/Navigation";
 import MoviePage from "./pages/MoviePage";
+import { Container } from "./components/common/Container/styled";
 
 function App() {
   return (
     <HashRouter>
       <Navigation />
+      <Container>
       <Routes>
         <Route path="/movies" element={<Movies />} />
         <Route path="/movie-page" element={<MoviePage />} />
         <Route path="/people" element={<People />} />
         <Route path="/" element={<Navigate to="/movies" />} />
       </Routes>
+      </Container>
     </HashRouter>
   );
 }

900f7c11e3ed2d82e201f12d07c57ac92dfb5da3
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2023-11-22 03:25:33 +0100

    add Container to movie page (resolve conflict)

--- src/pages/MoviePage/MoviePage.jsx
+++ src/pages/MoviePage/MoviePage.jsx
@@ -14,6 +14,7 @@ import {
   Votes,
   RatingTopContent,
   ContainerRateText,
+  Container
 } from "./styled";
 import { useDispatch, useSelector } from "react-redux";
 import { PersonTile } from "../../components/common/Tile/PersonTile/PersonTile";

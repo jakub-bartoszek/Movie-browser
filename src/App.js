0e763d7c1a3d70dba836406027d4db866b5cf196
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2023-11-23 01:02:50 +0100

    add new rout to person page

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import Movies from "./pages/Movies/Movies";
 import People from "./pages/People/People";
 import { Navigation } from "./components/common/Navigation/Navigation";
 import MoviePage from "./pages/MoviePage/MoviePage";
-import { toMoviePage, toMovies, toPeople } from "./routes";
+import { toMoviePage, toMovies, toPeople, toPersonPage } from "./routes";
 import { Container } from "./components/common/Container/styled";
+import PersonPage from "./pages/PersonPage/PersonPage";
 
 function App() {
   return (
     <HashRouter>
       <Navigation />
       <Container>
-      <Routes>
-        <Route path={toMoviePage()} element={<MoviePage />} />
-        <Route path={toMovies()} element={<Movies />} />
-        <Route path={toPeople()} element={<People />} />
-        <Route path="/" element={<Movies />} />
-      </Routes>
+        <Routes>
+          <Route path={toPersonPage()} element={<PersonPage />} />
+          <Route path={toMoviePage()} element={<MoviePage />} />
+          <Route path={toMovies()} element={<Movies />} />
+          <Route path={toPeople()} element={<People />} />
+          <Route path="/" element={<Movies />} />
+        </Routes>
       </Container>
     </HashRouter>
   );

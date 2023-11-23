02828a5c8d141e0a42125c2f5a19239a4959d003
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-11-23 22:28:00 +0100

    more responsive movie page

--- src/components/common/Tile/Tile.jsx
+++ src/components/common/Tile/Tile.jsx
@@ -1,59 +1,73 @@
 import {
-  Container,
-  Content,
-  Header,
-  Image,
-  Year,
-  Release,
-  ReleaseDate,
-  GreyRelease,
-  Genres,
-  Genre,
-  Rating,
-  IconStar,
-  Rate,
-  Votes,
-  Discription,
-  ContainerRateText,
+	Container,
+	Content,
+	Title,
+	Image,
+	Year,
+	Release,
+	Genres,
+	Genre,
+	Rating,
+	IconStar,
+	Rate,
+	Votes,
+	Discription,
+	ImageWrapper,
+	Production,
+	OutOf,
+	ReleaseDate
 } from "./styled";
 
-export const Tile = ({ poster_path, title, release_date, production_countries, vote_average, votes, overview, genres }) => {
-  const isMobile = window.innerWidth < 767;
-  const countryNames = production_countries?.map(country => (isMobile ? country.iso_3166_1 : country.name)) || [];
-  const releaseDate = new Date(release_date);
-  const year = releaseDate.getFullYear().toString();
+export const Tile = ({
+	poster_path,
+	title,
+	release_date,
+	production_countries,
+	vote_average,
+	votes,
+	overview,
+	genres
+}) => {
+	const isMobile = window.innerWidth < 767;
+	const countryNames =
+		production_countries?.map((country) =>
+			isMobile ? country.iso_3166_1 : country.name
+		) || [];
+	const releaseDate = new Date(release_date);
+	const year = releaseDate.getFullYear().toString();
 
-  return (
-    <Container>
-      <Image src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt="" />
-      <Content>
-        <Header>{title}</Header>
-        <Year>{year}</Year>
-        <Release>
-          <GreyRelease>Production:</GreyRelease>
-          {countryNames.toString().replace(",", ', ')}
-        </Release>
-        <ReleaseDate>
-          <GreyRelease>Release date:</GreyRelease>
-          {release_date}
-        </ReleaseDate>
-        <Genres>
-          {genres && genres.map(genre => (
-            <Genre key={genre.id}>{genre.name}</Genre>
-          ))}
-        </Genres>
-        <Rating>
-          <IconStar />
-          <ContainerRateText>
-            {vote_average ? vote_average.toFixed(2).toString().replace(".", ',') : "No votes yet"}
-            <Rate>/ 10</Rate>
-            <Votes>{votes}&nbsp;votes</Votes>
-          </ContainerRateText>
-        </Rating>
-      </Content>
-      <Discription>
-        {overview}
-      </Discription>
-    </Container>
-  );
+	return (
+		<Container>
+			<ImageWrapper>
+				<Image
+					src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+					alt=""
+				/>
+			</ImageWrapper>
+			<Content>
+				<Title>{title}</Title>
+				<Year>{year}</Year>
+				<Release>
+					<Production>{countryNames.toString().replace(",", ", ")}</Production>
+					<ReleaseDate>{release_date.toString().replaceAll("-", ".")}</ReleaseDate>
+				</Release>
+				<Genres>
+					{genres &&
+						genres.map((genre) => <Genre key={genre.id}>{genre.name}</Genre>)}
+				</Genres>
+
+				{vote_average ? (
+					<Rating>
+						<IconStar />
+						<Rate>{vote_average.toFixed(2).toString().replace(".", ",")}</Rate>
+						<OutOf>/ 10</OutOf>
+						<Votes>{votes} votes</Votes>
+					</Rating>
+				) : (
+					<Votes>No votes</Votes>
+				)}
+			</Content>
+			<Discription>{overview}</Discription>
+		</Container>
+	);
 };

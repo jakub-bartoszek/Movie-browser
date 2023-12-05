025ac4624a1e344f5d109900801b849576d3dbbe
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-05 01:00:02 +0100

    fixed logic with search param in url

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -2,21 +2,21 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { SmallTile } from "../../components/common/SmallTile/SmallTile";
 import {
-  fetchPopularMovies,
-  fetchSearchResults,
-  selectMovies,
-  selectStatus,
-  setPage,
-  selectPage,
-  selectTotalPages,
+ fetchPopularMovies,
+ fetchSearchResults,
+ selectMovies,
+ selectStatus,
+ setPage,
+ selectPage,
+ selectTotalPages
 } from "../../utils/redux/moviesSlice";
 import {
-  selectCategory,
-  selectSearchPage,
-  selectSearchQuery,
-  selectSearchTotalPages,
-  setCategory,
-  setSearchQuery,
+ selectCategory,
+ selectSearchPage,
+ selectSearchQuery,
+ selectSearchTotalPages,
+ setCategory,
+ setSearchQuery
 } from "../../utils/redux/searchSlice";
 
 import { Container, Content, Header, StyledLink } from "./styled";
@@ -25,82 +25,85 @@ import { NoResult } from "../NoResult/NoResult";
 import { Error } from "../Error/Error";
 import { StyledLoader } from "../../components/common/StyledLoader/styled";
 import { PaginationMovies } from "../../components/common/Navigation/Pagination/PaginationMovies";
+import { useSearchParams } from "react-router-dom";
 
 export default function Movies() {
-  const dispatch = useDispatch();
-  const movies = useSelector(selectMovies);
-  const searchQuery = useSelector(selectSearchQuery);
-  const status = useSelector(selectStatus);
-  const category = useSelector(selectCategory);
-  const page = useSelector(selectPage);
-  const totalPages = useSelector(selectTotalPages);
-  const searchPage = useSelector(selectSearchPage);
-  const searchTotalPages = useSelector(selectSearchTotalPages);
+ const dispatch = useDispatch();
+ const movies = useSelector(selectMovies);
+ const searchQuery = useSelector(selectSearchQuery);
+ const status = useSelector(selectStatus);
+ const category = useSelector(selectCategory);
+ const page = useSelector(selectPage);
+ const totalPages = useSelector(selectTotalPages);
+ const searchPage = useSelector(selectSearchPage);
+ const searchTotalPages = useSelector(selectSearchTotalPages);
+ const [searchParams, setSearchParams] = useSearchParams();
 
-  useEffect(() => {
-    if (searchQuery) {
-      dispatch(
-        fetchSearchResults({
-          category: "movie",
-          searchQuery: searchQuery,
-          page: searchPage,
-        })
-      );
-    } else {
-      dispatch(fetchPopularMovies({ category: "movie", page: page }));
-    }
-  }, [searchQuery, page, searchPage, dispatch]);
+ useEffect(() => {
+  if (searchParams.get("search")) {
+   dispatch(
+    fetchSearchResults({
+     category: "movie",
+     searchQuery: searchParams.get("search"),
+     page: searchPage
+    })
+   );
+  } else {
+   dispatch(fetchPopularMovies({ category: "movie", page: page }));
+  }
+ }, [searchParams, searchQuery, page, searchPage, dispatch]);
 
-  useEffect(() => {
-    dispatch(setCategory("movies"));
-    dispatch(setSearchQuery(""));
-    dispatch(setPage(1));
-  }, [dispatch, category]);
+ useEffect(() => {
+  dispatch(setCategory("movies"));
+  dispatch(setPage(1));
+ }, [dispatch, category]);
 
-  return (
-    <Container>
-      {status !== "error" && (
-        <Header>
-          {!searchQuery
-            ? `Popular movies`
-            : (searchQuery && movies.length) > 0
-            ? `Search results for "${searchQuery}" (${movies.length})`
-            : `Sorry, there are no results for "${searchQuery}"`}
-        </Header>
-      )}
-      {
-        {
-          loading: <StyledLoader />,
-          error: <Error />,
-          success: (
-            <>
-              {movies.length > 0 ? (
-                <>
-                  <Content>
-                    {movies?.map((movie) => (
-                      <StyledLink
-                        to={toMoviePage({ id: movie.id })}
-                        key={movie.id}
-                      >
-                        <SmallTile movie={movie} />
-                      </StyledLink>
-                    ))}
-                  </Content>
-                  <PaginationMovies
-                    page={page}
-                    totalPages={totalPages}
-                    searchTotalPages={searchTotalPages}
-                    searchPage={searchPage}
-                    searchQuery={searchQuery}
-                  />
-                </>
-              ) : (
-                <NoResult />
-              )}
-            </>
-          ),
-        }[status]
-      }
-    </Container>
-  );
+ return (
+  <Container>
+   {status !== "error" && (
+    <Header>
+     {!searchQuery
+      ? `Popular movies`
+      : status === "loading"
+      ? `Search results for "${searchQuery}"`
+      : status === "success" && movies.length > 0
+      ? `Search results for "${searchQuery}" (${movies.length})`
+      : `Sorry, there are no results for "${searchQuery}"`}
+    </Header>
+   )}
+   {
+    {
+     loading: <StyledLoader />,
+     error: <Error />,
+     success: (
+      <>
+       {movies.length > 0 ? (
+        <>
+         <Content>
+          {movies?.map((movie) => (
+           <StyledLink
+            to={toMoviePage({ id: movie.id })}
+            key={movie.id}
+           >
+            <SmallTile movie={movie} />
+           </StyledLink>
+          ))}
+         </Content>
+         <PaginationMovies
+          page={page}
+          totalPages={totalPages}
+          searchTotalPages={searchTotalPages}
+          searchPage={searchPage}
+          searchQuery={searchQuery}
+         />
+        </>
+       ) : (
+        <NoResult />
+       )}
+      </>
+     )
+    }[status]
+   }
+  </Container>
+ );
 }
--- src/pages/People/People.jsx
+++ src/pages/People/People.jsx
@@ -1,21 +1,21 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Container, Content, Header } from "./styled";
 import {
-  fetchPopularPeople,
-  fetchSearchResults,
-  selectPeople,
-  selectStatus,
-  selectTotalPages,
-  selectPage,
-  setPage,
+ fetchPopularPeople,
+ fetchSearchResults,
+ selectPeople,
+ selectStatus,
+ selectTotalPages,
+ selectPage,
+ setPage
 } from "../../utils/redux/peopleSlice";
 import {
-  selectCategory,
-  selectSearchPage,
-  selectSearchQuery,
-  selectSearchTotalPages,
-  setCategory,
-  setSearchQuery,
+ selectCategory,
+ selectSearchPage,
+ selectSearchQuery,
+ selectSearchTotalPages,
+ setCategory,
+ setSearchQuery
 } from "../../utils/redux/searchSlice";
 import { nanoid } from "nanoid";
 import { PersonTile } from "../../components/common/PersonTile/PersonTile";
@@ -24,77 +24,83 @@ import { Error } from "../Error/Error";
 import { StyledLoader } from "../../components/common/StyledLoader/styled";
 import { useEffect } from "react";
 import { PaginationPeople } from "../../components/common/Navigation/Pagination/PaginationPeople";
+import { useSearchParams } from "react-router-dom";
 
 export default function People() {
-  const dispatch = useDispatch();
-  const people = useSelector(selectPeople);
-  const status = useSelector(selectStatus);
-  const searchQuery = useSelector(selectSearchQuery);
-  const category = useSelector(selectCategory);
-  const page = useSelector(selectPage);
-  const totalPages = useSelector(selectTotalPages);
-  const searchPage = useSelector(selectSearchPage);
-  const searchTotalPages = useSelector(selectSearchTotalPages);
+ const dispatch = useDispatch();
+ const people = useSelector(selectPeople);
+ const status = useSelector(selectStatus);
+ const searchQuery = useSelector(selectSearchQuery);
+ const category = useSelector(selectCategory);
+ const page = useSelector(selectPage);
+ const totalPages = useSelector(selectTotalPages);
+ const searchPage = useSelector(selectSearchPage);
+ const searchTotalPages = useSelector(selectSearchTotalPages);
+ const [searchParams, setSearchParams] = useSearchParams();
 
-  useEffect(() => {
-    if (searchQuery) {
-      dispatch(
-        fetchSearchResults({
-          category: "person",
-          searchQuery: searchQuery,
-          page: searchPage,
-        })
-      );
-    } else {
-      dispatch(fetchPopularPeople({ category: "person", page: page }));
-    }
-  }, [searchQuery, dispatch, page, searchPage]);
+ useEffect(() => {
+  if (searchParams.get("search")) {
+   dispatch(
+    fetchSearchResults({
+     category: "person",
+     searchQuery: searchParams.get("search"),
+     page: searchPage
+    })
+   );
+  } else {
+   dispatch(fetchPopularPeople({ category: "person", page: page }));
+  }
+ }, [searchParams, searchQuery, page, searchPage, dispatch]);
 
-  useEffect(() => {
-    dispatch(setCategory("people"));
-    dispatch(setSearchQuery(""));
-    dispatch(setPage(1));
-  }, [dispatch, category]);
+ useEffect(() => {
+  dispatch(setCategory("people"));
+  dispatch(setPage(1));
+ }, [dispatch, category]);
 
-  return (
-    <Container>
-      {status !== "error" && (
-        <Header>
-          {!searchQuery
-            ? `Popular people`
-            : (searchQuery && people.length) > 0
-            ? `Search results for "${searchQuery}" (${people.length})`
-            : `Sorry, there are no results for "${searchQuery}"`}
-        </Header>
-      )}
-      {
-        {
-          loading: <StyledLoader />,
-          error: <Error />,
-          success: (
-            <>
-              {people.length > 0 ? (
-                <>
-                  <Content>
-                    {people?.map((member) => (
-                      <PersonTile key={nanoid()} member={member} />
-                    ))}
-                  </Content>
-                  <PaginationPeople
-                    page={page}
-                    totalPages={totalPages}
-                    searchPage={searchPage}
-                    searchTotalPages={searchTotalPages}
-                    searchQuery={searchQuery}
-                  />
-                </>
-              ) : (
-                <NoResult />
-              )}
-            </>
-          ),
-        }[status]
-      }
-    </Container>
-  );
+ return (
+  <Container>
+   {status !== "error" && (
+    <Header>
+     {!searchQuery
+      ? `Popular people`
+      : status === "loading"
+      ? `Search results for "${searchQuery}"`
+      : status === "success" && people.length > 0
+      ? `Search results for "${searchQuery}" (${people.length})`
+      : `Sorry, there are no results for "${searchQuery}"`}
+    </Header>
+   )}
+   {
+    {
+     loading: <StyledLoader />,
+     error: <Error />,
+     success: (
+      <>
+       {people.length > 0 ? (
+        <>
+         <Content>
+          {people?.map((member) => (
+           <PersonTile
+            key={nanoid()}
+            member={member}
+           />
+          ))}
+         </Content>
+         <PaginationPeople
+          page={page}
+          totalPages={totalPages}
+          searchPage={searchPage}
+          searchTotalPages={searchTotalPages}
+          searchQuery={searchQuery}
+         />
+        </>
+       ) : (
+        <NoResult />
+       )}
+      </>
+     )
+    }[status]
+   }
+  </Container>
+ );
 }

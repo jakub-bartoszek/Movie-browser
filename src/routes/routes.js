6ff5ff3e3cfa4dfd110d6804ab3ec4e8aef7f5ca
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-06 01:44:40 +0100

    changed person to people in routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
 export const toMovies = () => "/movies";
 export const toPeople = () => "/people";
 export const toMoviePage = ({ id } = { id: ":id" }) => `/movies/${id}`;
-export const toPersonPage = ({ id } = { id: ":id" }) => `/person/${id}`;
+export const toPersonPage = ({ id } = { id: ":id" }) => `/people/${id}`;

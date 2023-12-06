cf72f9a30fe709ca9037e63fc3a4072ae0af9a67
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-06 01:43:55 +0100

    navigating to main page when clicking searchbar

--- src/components/common/Navigation/SearchBar/SearchBar.jsx
+++ src/components/common/Navigation/SearchBar/SearchBar.jsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router";
 import { SearchInput, StyledSearchIcon, Wrapper } from "./styled";
 import { useSearch } from "./useSearch";
+import { selectCategory } from "../../../../utils/redux/dataSlice";
+import { useSelector } from "react-redux";
 
 export const SearchBar = () => {
- const category = "movies";
+ const category = useSelector(selectCategory);
  const { onInputChange, searchQuery } = useSearch();
+ const navigate = useNavigate()
  
  return (
   <Wrapper>
@@ -12,6 +16,7 @@ export const SearchBar = () => {
     placeholder={`Search for ${category}...`}
     value={searchQuery || ""}
     onChange={onInputChange}
+    onClick={() => navigate(`/${category}`)}
    />
   </Wrapper>
  );

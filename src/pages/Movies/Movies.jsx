7986f6a3baf96e615e53e568d06205b24a45df0d
jakub-bartoszek-developer <k.bartoszek507@wp.pl>
2024-04-16 17:39:38 +0200

    moved Header to renderContent function

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,68 +1,82 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { SmallTile } from "../../components/common/SmallTile/SmallTile";
+import { nanoid } from "nanoid";
+import { useSearchParams } from "react-router-dom";
 import {
  fetchPopularMovies,
  selectMovies,
  selectStatus,
  selectTotalPages,
  selectCategory,
- selectSearchTotalPages,
  setCategory,
  fetchMoviesSearchResults
 } from "../../utils/redux/dataSlice";
-
+import { SmallTile } from "../../components/common/SmallTile/SmallTile";
 import { Container, Content, Header } from "./styled";
+import { Pagination } from "../../components/common/Pagination/Pagination";
 import { NoResult } from "../NoResult/NoResult";
 import { Error } from "../Error/Error";
 import { StyledLoader } from "../../components/common/StyledLoader/styled";
-import { Pagination } from "../../components/common/Pagination/Pagination";
-import { useSearchParams } from "react-router-dom";
-import { nanoid } from "nanoid";
 
 export default function Movies() {
  const dispatch = useDispatch();
+ const [searchParams] = useSearchParams();
  const movies = useSelector(selectMovies);
  const status = useSelector(selectStatus);
  const category = useSelector(selectCategory);
- const totalPages = useSelector(selectTotalPages);
- const searchPage = useSelector(selectSearchTotalPages);
- const searchTotalPages = useSelector(selectSearchTotalPages);
- const [searchParams, setSearchParams] = useSearchParams();
  const searchQuery = searchParams.get("search") || "";
- const page = searchParams.get("page") || "1";
+ const page = parseInt(searchParams.get("page") || "1");
+ const totalPages = useSelector(selectTotalPages);
 
  useEffect(() => {
   if (searchQuery) {
    dispatch(
     fetchMoviesSearchResults({
      category: "movie",
-     searchQuery: searchQuery,
-     page: page
+     searchQuery,
+     page
     })
    );
   } else {
-   dispatch(fetchPopularMovies({ category: "movie", page: page }));
+   dispatch(fetchPopularMovies({ category: "movie", page }));
   }
- }, [searchQuery, page, searchPage, dispatch]);
+ }, [searchQuery, page, dispatch]);
 
  useEffect(() => {
   dispatch(setCategory("movies"));
  }, [dispatch, category]);
+
+ function renderHeaderText() {
+  if (!searchQuery) {
+   return "Popular movies";
+  } else if (status === "loading") {
+   return `Search results for "${searchQuery}"`;
+  } else if (status === "success" && movies?.length > 0) {
+   return `Search results for "${searchQuery}" (${movies.length})`;
+  } else {
+   return `Sorry, there are no results for "${searchQuery}"`;
+  }
+ }
 
  const renderContent = () => {
   switch (status) {
    case "error":
     return <Error />;
    case "loading":
-    return <StyledLoader />;
+    return (
+     <>
+      <Header>{renderHeaderText()}</Header>
+      <StyledLoader />
+     </>
+    );
    case "success":
     return (
      <>
-      {movies.length > 0 ? (
+      <Header>{renderHeaderText()}</Header>
+      {movies?.length > 0 ? (
        <>
         <Content>
-         {movies?.map((movie) => (
+         {movies.map((movie) => (
           <SmallTile
            key={nanoid()}
            movie={movie}
@@ -70,11 +84,8 @@ export default function Movies() {
          ))}
         </Content>
         <Pagination
-         page={page}
          totalPages={totalPages}
-         searchTotalPages={searchTotalPages}
-         searchPage={searchPage}
-         searchQuery={searchQuery}
+         page={page}
         />
        </>
       ) : (
@@ -87,22 +98,5 @@ export default function Movies() {
   }
  };
 
- function renderHeaderText() {
-  if (!searchQuery) {
-   return "Popular movies";
-  } else if (status === "loading") {
-   return `Search results for "${searchQuery}"`;
-  } else if (status === "success" && movies.length > 0) {
-   return `Search results for "${searchQuery}" (${movies.length})`;
-  } else {
-   return `Sorry, there are no results for "${searchQuery}"`;
-  }
- }
-
- return (
-  <Container>
-   <Header>{renderHeaderText()}</Header>
-   {renderContent()}
-  </Container>
- );
+ return <Container>{renderContent()}</Container>;
 }

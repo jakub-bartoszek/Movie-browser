c57ce6d16b73b16f4831c95995945f91dc1ecbfc
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-04-18 23:27:36 +0200

    removed curly braces and return

--- src/components/common/PersonTile/PersonTile.jsx
+++ src/components/common/PersonTile/PersonTile.jsx
@@ -1,36 +1,21 @@
-import {
- Image,
- ImageWrapper,
- Character,
- Name,
- Wrapper,
- FilmName,
- PersonIcon,
-} from "./styled";
+import { Image, ImageWrapper, Character, Name, Wrapper, FilmName, PersonIcon } from "./styled";
 import { toPersonPage } from "../../../routes/routes";
 
-export const PersonTile = ({ person, filmName }) => {
-
-console.log(person)
-
- return (
-  <Wrapper
-   to={toPersonPage({ id: person.id })}
-  >
-   <ImageWrapper>
-    {person.profile_path ? (
-     <Image
-      src={`https://image.tmdb.org/t/p/w500/${person.profile_path}`}
-      alt=""
-     />
-    ) : (
-     <PersonIcon />
-    )}
-   </ImageWrapper>
-   <Character>
-    <Name>{person.name}</Name>
-    {filmName && <FilmName>{filmName}</FilmName>}
-   </Character>
-  </Wrapper>
- );
-};
+export const PersonTile = ({ person, filmName }) => (
+ <Wrapper to={toPersonPage({ id: person.id })}>
+  <ImageWrapper>
+   {person.profile_path ? (
+    <Image
+     src={`https://image.tmdb.org/t/p/w500/${person.profile_path}`}
+     alt=""
+    />
+   ) : (
+    <PersonIcon />
+   )}
+  </ImageWrapper>
+  <Character>
+   <Name>{person.name}</Name>
+   {filmName && <FilmName>{filmName}</FilmName>}
+  </Character>
+ </Wrapper>
+);

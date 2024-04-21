7988775bb2be93c60dc8465a4a108b4f1cdc83ca
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-04-21 13:29:27 +0200

    simplified syntax of renderContent

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -2,30 +2,24 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { nanoid } from "nanoid";
 import { useSearchParams } from "react-router-dom";
+import { selectStatus, selectTotalPages, setCategory } from "../../utils/redux/dataSlice";
 import {
- selectStatus,
- selectTotalPages,
- selectCategory,
- setCategory
-} from "../../utils/redux/dataSlice";
+ fetchMoviesSearchResults,
+ fetchPopularMovies,
+ selectMovies
+} from "../../utils/redux/moviesSlice";
 import { SmallTile } from "../../components/common/SmallTile/SmallTile";
 import { Container, Content, Header } from "./styled";
 import { Pagination } from "../../components/common/Pagination/Pagination";
 import { NoResult } from "../NoResult/NoResult";
 import { Error } from "../Error/Error";
 import { StyledLoader } from "../../components/common/StyledLoader/styled";
-import {
- fetchMoviesSearchResults,
- fetchPopularMovies,
- selectMovies
-} from "../../utils/redux/moviesSlice";
 
 export default function Movies() {
  const dispatch = useDispatch();
  const [searchParams] = useSearchParams();
  const movies = useSelector(selectMovies);
  const status = useSelector(selectStatus);
- const category = useSelector(selectCategory);
  const searchQuery = searchParams.get("search") || "";
  const page = parseInt(searchParams.get("page") || "1");
  const totalPages = useSelector(selectTotalPages);
@@ -46,7 +40,7 @@ export default function Movies() {
 
  useEffect(() => {
   dispatch(setCategory("movies"));
- }, [dispatch, category]);
+ }, []);
 
  function renderHeaderText() {
   if (!searchQuery) {
@@ -55,6 +49,8 @@ export default function Movies() {
    return `Search results for "${searchQuery}"`;
   } else if (status === "success" && movies?.length > 0) {
    return `Search results for "${searchQuery}" (${movies.length})`;
+  } else if (status === "error") {
+   return null;
   } else {
    return `Sorry, there are no results for "${searchQuery}"`;
   }
@@ -65,40 +61,37 @@ export default function Movies() {
    case "error":
     return <Error />;
    case "loading":
-    return (
-     <>
-      <Header>{renderHeaderText()}</Header>
-      <StyledLoader />
-     </>
-    );
+    return <StyledLoader />;
    case "success":
-    return (
-     <>
-      <Header>{renderHeaderText()}</Header>
-      {movies?.length > 0 ? (
-       <>
-        <Content>
-         {movies.map((movie) => (
-          <SmallTile
-           key={nanoid()}
-           movie={movie}
-          />
-         ))}
-        </Content>
-        <Pagination
-         totalPages={totalPages}
-         page={page}
-        />
-       </>
-      ) : (
-       <NoResult />
-      )}
-     </>
-    );
+    if (movies && movies.length > 0) {
+     return (
+      <>
+       <Content>
+        {movies.map((movie) => (
+         <SmallTile
+          key={movie.id}
+          movie={movie}
+         />
+        ))}
+       </Content>
+       <Pagination
+        totalPages={totalPages}
+        page={page}
+       />
+      </>
+     );
+    } else {
+     return <NoResult />;
+    }
    default:
     return null;
   }
  };
 
- return <Container>{renderContent()}</Container>;
+ return (
+  <Container>
+   <Header>{renderHeaderText()}</Header>
+   {renderContent()}
+  </Container>
+ );
 }

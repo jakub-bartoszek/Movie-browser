b1ba7be148c85e2b85085726e466c54f3fb8a1a5
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-04-21 13:35:13 +0200

    added renderContent() function

--- src/pages/MoviePage/MoviePage.jsx
+++ src/pages/MoviePage/MoviePage.jsx
@@ -1,6 +1,14 @@
 import { useParams } from "react-router";
 import { useEffect } from "react";
-import { Tile } from "../../components/common/Tile/Tile";
+import { useDispatch, useSelector } from "react-redux";
+import { nanoid } from "nanoid";
+import {
+ fetchMovieCredits,
+ fetchMovieDetails,
+ selectMovieCredits,
+ selectMovieDetails
+} from "../../utils/redux/moviesSlice";
+import { selectStatus, setCategory } from "../../utils/redux/dataSlice";
 import {
  MainWrapper,
  Header,
@@ -16,95 +24,90 @@ import {
  ContainerRateText,
  Container
 } from "./styled";
-import { useDispatch, useSelector } from "react-redux";
+import { Tile } from "../../components/common/Tile/Tile";
 import { PersonTile } from "../../components/common/PersonTile/PersonTile";
-import { selectStatus } from "../../utils/redux/dataSlice";
 import { StyledLoader } from "../../components/common/StyledLoader/styled";
 import { Error } from "../Error/Error";
-import { nanoid } from "nanoid";
-import {
- fetchMovieCredits,
- fetchMovieDetails,
- selectMovieCredits,
- selectMovieDetails
-} from "../../utils/redux/moviesSlice";
 
 export default function MoviePage() {
- const { id } = useParams();
  const dispatch = useDispatch();
+ const { id } = useParams();
  const movieDetails = useSelector(selectMovieDetails);
  const status = useSelector(selectStatus);
  const movieCredits = useSelector(selectMovieCredits);
-
- console.log(movieCredits)
 
  useEffect(() => {
   dispatch(fetchMovieDetails(id));
   dispatch(fetchMovieCredits(id));
  }, [dispatch, id]);
 
- return (
-  <Container>
-   {
-    {
-     loading: <StyledLoader />,
-     error: <Error />,
-     success: (
-      <>
-       <MainWrapper>
-        <Header $backdropPath={`https://image.tmdb.org/t/p/original${movieDetails.backdrop_path}`}>
-         <RatingTopContent>
-          <LongTitle>{movieDetails.title}</LongTitle>
-          <Rating>
-           <IconStar />
-           <ContainerRateText>
-            {movieDetails.vote_average
-             ? movieDetails.vote_average.toFixed(2).toString().replace(".", ",")
-             : "No votes yet"}
-            <Rate>/ 10</Rate>
-           </ContainerRateText>
+ useEffect(() => {
+  dispatch(setCategory("movies"));
+ }, []);
 
-           <Votes>{movieDetails.vote_count}&nbsp;votes</Votes>
-          </Rating>
-         </RatingTopContent>
-        </Header>
-       </MainWrapper>
+ const renderContent = () => {
+  switch (status) {
+   case "error":
+    return <Error />;
+   case "loading":
+    return <StyledLoader />;
+   case "success":
+    return (
+     <>
+      <MainWrapper>
+       <Header $backdropPath={`https://image.tmdb.org/t/p/original${movieDetails.backdrop_path}`}>
+        <RatingTopContent>
+         <LongTitle>{movieDetails.title}</LongTitle>
+         <Rating>
+          <IconStar />
+          <ContainerRateText>
+           {movieDetails.vote_average
+            ? movieDetails.vote_average.toFixed(2).toString().replace(".", ",")
+            : "No votes yet"}
+           <Rate>/ 10</Rate>
+          </ContainerRateText>
+          <Votes>{movieDetails.vote_count}&nbsp;votes</Votes>
+         </Rating>
+        </RatingTopContent>
+       </Header>
+      </MainWrapper>
+      <Wrapper>
+       <Tile
+        poster_path={movieDetails.poster_path}
+        title={movieDetails.title}
+        release_date={movieDetails.release_date}
+        production_countries={movieDetails.production_countries}
+        vote_average={movieDetails.vote_average}
+        votes={movieDetails.vote_count}
+        overview={movieDetails.overview}
+        genres={movieDetails.genres}
+       />
+       <SectionTitle>Cast</SectionTitle>
+       <Section>
+        {movieCredits.cast.map((person) => (
+         <PersonTile
+          key={nanoid()}
+          person={person}
+          filmName={person.character ? person.character : "Unknown Character"}
+         />
+        ))}
+       </Section>
+       <SectionTitle>Crew</SectionTitle>
+       <Section>
+        {movieCredits.crew.map((person) => (
+         <PersonTile
+          key={nanoid()}
+          person={person}
+         />
+        ))}
+       </Section>
+      </Wrapper>
+     </>
+    );
+   default:
+    return null;
+  }
+ };
 
-       <Wrapper>
-        <Tile
-         poster_path={movieDetails.poster_path}
-         title={movieDetails.title}
-         release_date={movieDetails.release_date}
-         production_countries={movieDetails.production_countries}
-         vote_average={movieDetails.vote_average}
-         votes={movieDetails.vote_count}
-         overview={movieDetails.overview}
-         genres={movieDetails.genres}
-        />
-        <SectionTitle>Cast</SectionTitle>
-        <Section>
-         {movieCredits.cast.map((person) => (
-          <PersonTile
-           key={nanoid()}
-           person={person}
-           filmName={person.character ? person.character : "Unknown Character"}
-          />
-         ))}
-        </Section>
-        <SectionTitle>Crew</SectionTitle>
-        <Section>
-         {movieCredits.crew.map((person) => (
-          <PersonTile
-           key={nanoid()}
-           person={person}
-          />
-         ))}
-        </Section>
-       </Wrapper>
-      </>
-     )
-    }[status]
-   }
-  </Container>
- );
+ return <Container>{renderContent()}</Container>;
 }
